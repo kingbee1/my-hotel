89c03119f4044c2d0df645cc1fd12e22fc748a5a
Abiodun <abiodunhodonu@gmail.com>
2020-07-31 21:49:26 +0100

    created components for the hotel

---
+++ src/pages/Error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+export default function Error() {
+	return <Hero> 
+		<Banner title='404' subtitle="could not find page">
+		<Link to='/' className='btn-primary'>
+		go back to home!
+		</Link>
+		</Banner>
+	</Hero>
+}
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleRoom = () => {
+	return (
+		<div>
+		single ladies say hi too.
+		</div>
+		)
+}
+
+export default SingleRoom
